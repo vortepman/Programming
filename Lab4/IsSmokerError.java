4161f1ec43b8c9cf45aab3bd62cab89eb5b2e370
vortepman <77311483+vortepman@users.noreply.github.com>
2021-01-15 20:03:57 +0700

    Update IsSmokerError.java

--- Lab4/IsSmokerError.java
+++ Lab4/IsSmokerError.java
@@ -1,4 +1,8 @@
 public class IsSmokerError extends RuntimeException{
+   
+/**
+An error generated when an erupting volcano is assigned the wrong field
+*/
     public IsSmokerError() {}
     public IsSmokerError(String message){
         super(message);

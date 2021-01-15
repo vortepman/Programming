158216cc84734713fd45299286f0c3abcdc6bc89
vortepman <77311483+vortepman@users.noreply.github.com>
2021-01-15 17:32:26 +0300

    Update PlotError.java

--- Lab4/PlotError.java
+++ Lab4/PlotError.java
@@ -4,7 +4,6 @@ public class PlotError extends Exception {
 An error that occurs when the ship is not in the water when it should be there.
 */
     
-    public PlotError() {}
     public PlotError (String message) {
         super(message);
     }

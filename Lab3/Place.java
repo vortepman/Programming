69406c6ba07ea9e2e99995a62a46295da15d6be7
vortepman <77311483+vortepman@users.noreply.github.com>
2021-01-15 15:28:29 +0300

    Create Place.java

---
+++ Lab3/Place.java
@@ -0,0 +1,15 @@
+public enum Place {
+    EREBUS("к Эребусу"),
+    TERROR("к Террору"),
+    WATER("в открытые воды");
+
+    private final String position;
+
+    Place(String place) {
+        this.position = place;
+    }
+
+    public String getPlace() {
+        return position;
+    }
+}

fdfb0fd54d70d4497cc83dc6e25478fbc0fe0bff
vortepman <77311483+vortepman@users.noreply.github.com>
2021-01-15 15:28:25 +0300

    Create InterVulcan.java

---
+++ Lab3/InterVulcan.java
@@ -0,0 +1,14 @@
+public interface InterVulcan {
+
+    void setName(String name);
+    void endOfEruption();
+    String getName();
+    void setHasLand(boolean hasLand);
+    boolean getHasLand();
+    void setSmoker(boolean smoker);
+    boolean getSmoker();
+    void setHeight(int height);
+    int getHeight();
+    void startOfEruption();
+
+}

a766b41f4eb308c68e8e1a4936bb8487380c0a13
vortepman <77311483+vortepman@users.noreply.github.com>
2021-09-24 17:22:21 +0300

    Create MusicGenre.java

---
+++ Lab7/src/given/MusicGenre.java
@@ -0,0 +1,43 @@
+package given;
+
+/**
+ * @author Petrov Ilya
+ * @version 1.1
+ * Enum class for describing music genre of element
+ */
+//public enum MusicGenre {
+//    ROCK("Rock"),
+//    JAZZ("Jazz"),
+//    BLUES("Blues");
+//
+//    private String genre;
+//
+//    MusicGenre(String genre) {
+//        this.genre = genre;
+//    }
+//
+//    MusicGenre() {}
+//
+//    public String getGenre() {
+//        return genre;
+//    }
+//
+//    public void setGenre(String genre) {
+//        this.genre = genre;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "MusicGenre{" +
+//                "genre='" + genre + '\'' +
+//                '}';
+//    }
+//}
+public enum MusicGenre {
+    ROCK,
+    JAZZ,
+    BLUES
+}
+
+
+

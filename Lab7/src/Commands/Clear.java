55a0ffa235e49037255611297086c4c7fde7266f
vortepman <77311483+vortepman@users.noreply.github.com>
2021-09-24 17:21:39 +0300

    Create Clear.java

---
+++ Lab7/src/Commands/Clear.java
@@ -0,0 +1,36 @@
+package Commands;
+
+import given.CollectionSupervisor;
+import given.DatabaseSupervisor;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * Class of command 'clear'
+ * @author Petrov Ilya
+ * @version 1.1
+ */
+public class Clear extends CommandSkeleton {
+
+    /**
+     * Method for executing this command
+     *
+     * @param databaseSupervisor collection
+     * @return String description of command
+     */
+    @Override
+    public String move(DatabaseSupervisor databaseSupervisor, String username){
+        Connection c = DatabaseSupervisor.getConnection();
+        try {
+            PreparedStatement ps = c.prepareStatement("delete from music_bands where user_name = ?");
+            ps.setString(1, username);
+            ps.executeUpdate();
+            databaseSupervisor.getMusicBands().clear();
+            return "Database successfully cleared.";
+        } catch (SQLException sqlException) {
+            return "Oh shit, I don't know how it happened)";
+        }
+    }
+}

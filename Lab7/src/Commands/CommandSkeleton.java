09c5614b58df01d01d955bcc99f2d385492b448c
vortepman <77311483+vortepman@users.noreply.github.com>
2021-09-24 17:21:42 +0300

    Create CommandSkeleton.java

---
+++ Lab7/src/Commands/CommandSkeleton.java
@@ -0,0 +1,33 @@
+package Commands;
+
+import given.CollectionSupervisor;
+import given.DatabaseSupervisor;
+
+/**
+ * @author Petrov Ilya
+ * @version 1.1
+ * Class from which all commands are inherited
+ */
+abstract public class CommandSkeleton {
+
+    /** Method for commands execution */
+    public Object move(String s) {
+        return null;
+    }
+    /** Method for commands execution */
+    public String move(){
+        return null;
+    }
+    /** Method for commands execution */
+    public Object move(DatabaseSupervisor databaseSupervisor, String s) {
+        return null;
+    }
+    /** Method for commands execution */
+    public Object move(String s, DatabaseSupervisor databaseSupervisor, String ss) {
+        return null;
+    }
+    /** Method for commands execution */
+    public Object move(DatabaseSupervisor databaseSupervisor) {
+        return null;
+    }
+}

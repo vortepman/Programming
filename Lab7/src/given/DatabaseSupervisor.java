3e9679b2576e43f609ff8e86da85d4e3275fddfe
vortepman <77311483+vortepman@users.noreply.github.com>
2021-10-02 16:05:13 +0300

    Update DatabaseSupervisor.java

--- Lab7/src/given/DatabaseSupervisor.java
+++ Lab7/src/given/DatabaseSupervisor.java
@@ -28,27 +28,28 @@ public class DatabaseSupervisor {
      * Field for saving date of initialization the collection
      */
     private LocalDateTime initializationDate;
-    //private static final String url = "jdbc:postgresql://localhost:********/studs";
-    private static final String url = "jdbc:postgresql://pg:********/studs";
-    private static final String user = "********";
-    private static final String pass =  "********";
-    private static final String schema =  "********";
+    //private static final String url = "jdbc:postgresql://localhost:9376/studs";
+    private static final String url = "jdbc:postgresql://pg:5432/studs";
+    private static final String user = "s312642";
+    private static final String pass =  "pkj919";
+    private static final String schema =  "s312642";
     private static Connection connection;
 
     // Constructor for establishing a connection to the database and fixing the start time
     public DatabaseSupervisor() {
         Connection c = DatabaseSupervisor.getConnection();
         initializationDate = LocalDateTime.now();
+        downloadElements(DatabaseSupervisor.getConnection(), getMusicBands());
     }
 
     /**
-     * Method downloading element to the client collection from database
+     * Method for downloading elements to the all users collection from database
      */
-    public static void downloadElements(Connection c, ArrayDeque<MusicBand> musicBands, String username) {
+    public static void downloadElements(Connection c, ArrayDeque<MusicBand> musicBands) {
         int downloads = 0;
         try {
-            PreparedStatement ps = c.prepareStatement("select * from music_bands where user_name = ?");
-            ps.setString(1, username);
+            PreparedStatement ps = c.prepareStatement("select * from music_bands");
+            //ps.setString(1, username);
             ResultSet rs = ps.executeQuery();
 
             while (rs.next()) {
@@ -87,6 +88,52 @@ public class DatabaseSupervisor {
             System.err.println(e.getMessage());
         }
     }
+
+    /**
+     * Method for downloading elements of one user from database
+     */
+    public static void downloadElementsForUser(Connection c, ArrayDeque<MusicBand> musicBands, String username) {
+        try {
+            PreparedStatement ps = c.prepareStatement("select * from music_bands where user_name = ?");
+            ps.setString(1, username);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Coordinates coordinates = new Coordinates(
+                        rs.getInt("x_coordinate"),
+                        (long) rs.getInt("y_coordinate")
+                );
+                Album album = new Album(
+                        rs.getString("best_album_name"),
+                        (long) rs.getInt("best_album_sales")
+                );
+                MusicGenre musicGenre;
+                if (rs.getString("genre").equals("ROCK")) {
+                    musicGenre = MusicGenre.ROCK;
+                } else if (rs.getString("genre").equals("JAZZ")) {
+                    musicGenre = MusicGenre.JAZZ;
+                } else {
+                    musicGenre = MusicGenre.BLUES;
+                }
+                MusicBand musicBand = new MusicBand(
+                        (long) rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("creation_date"),
+                        coordinates,
+                        rs.getInt("number_of_participants"),
+                        //MusicGenre.valueOf(rs.getString("genre")),
+                        //MusicGenre.ROCK,
+                        musicGenre,
+                        album
+                );
+                musicBands.add(musicBand);
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+
 
     /**
      * Method for getting the connection with database
@@ -139,25 +186,25 @@ public class DatabaseSupervisor {
         // Making a manual
         informationAboutCommands = new HashMap<>();
         informationAboutCommands.put("[help]", " - display help for available commands");
-        informationAboutCommands.put("[info]", " - display information about the collection" +
-                " (type, date of initialization, number of elements, etc.) to the standard output stream");
-        informationAboutCommands.put("[show]", " - print to standard output all elements of the your collection in string representation");
+        informationAboutCommands.put("[info]", " - display information about the server app and the database collection" +
+                " (type, date of server initialization, number of elements, etc.) to the standard output stream");
+        informationAboutCommands.put("[show]", " - print to standard output all elements from database of your collection in string representation");
+        informationAboutCommands.put("[show_all]", " - print to standard output all elements from database in string representation");
         informationAboutCommands.put("[back]", " - this command returns you to the registration field.");
-        informationAboutCommands.put("[show_all]", " - print to standard output all elements from database(collection of all users) in string representation");
-        informationAboutCommands.put("[add {element}]", " - add new element to the collection");
+        informationAboutCommands.put("[add {element}]", " - add new element to the database");
         informationAboutCommands.put("[update id {element}]", " - update the value of the collection element" +
                 " whose id is equal to the given. After entering the command," +
                 " you must enter the ID separated by a space. For example: update 1");
         informationAboutCommands.put("[remove id {element}]", " - remove an element from the collection by its ID." +
                 " After entering the command, you must enter the ID separated by a space. For example: remove_by_id 1");
-        informationAboutCommands.put("[clear]", " - clear the collection");
+        informationAboutCommands.put("[clear]", " - clear the your collection");
         informationAboutCommands.put("[execute_script file_name]", " - read and execute a script from specified file." +
                 " You should to enter path to file after entering a command.");
-        informationAboutCommands.put("[exit]", " - end the program (without saving to file)");
-        informationAboutCommands.put("[head]", " - display the first element of the collection");
-        informationAboutCommands.put("[remove_head]", " - display the first element of the collection and remove it");
+        informationAboutCommands.put("[exit]", " - end the program");
+        informationAboutCommands.put("[head]", " - display the first element of the your collection");
+        informationAboutCommands.put("[remove_head]", " - display the first element of the your collection and remove it");
         informationAboutCommands.put("[average_of_number_of_participants]", " - display the average value of the field" +
-                " numberOfParticipants for all elements of the collection");
+                " numberOfParticipants for all elements of the database");
         informationAboutCommands.put("[count_greater_than_number_of_participants numberOfParticipants]", " - display the number of" +
                 " elements, the value of the field numberOfParticipants of which is greater than the specified");
     }
